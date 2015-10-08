b3b79cd1f492ef53f4f13d939748ada100739dc0
altug <altugkarakurt@gmail.com>
2015-10-08 17:17:44 +0300

    Minor edit in Chordias import

--- ModeTonicEstimation/Chordia.py
+++ ModeTonicEstimation/Chordia.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import numpy as np
-from ModeTonicEstimation import ModeFunctions as mf
-from ModeTonicEstimation import PitchDistribution as pD
+import ModeFunctions as mf
+import PitchDistribution as pD
 import json
 import os
 import random
